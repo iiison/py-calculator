3d31584a30c2b459e7ef955f17fd15b094d2a985
iiison <i.bharat.soni@gmail.com>
2019-10-29 16:27:42 +0800

    FEAT: add exception handelling

--- input_taker.py
+++ input_taker.py
@@ -1,10 +1,18 @@
+from sty import fg, bg, rs
+
 def input_taker(val_count, val = 0):
     message = 'Enter Value {}: '.format(val_count) if val == 0 else 'Enter Value {}({}): '.format(val_count, val)
 
     value = input(message)
-    fmt_value = float(value) if value != '' else val
-    print(fmt_value)
+    fmt_value = ''
+    try:
+        fmt_value = float(value) if value != '' else val
+        print(fmt_value)
 
+    except:
+        print(fg.red + 'Think again, do you really think "{}" is a number?'.format(value) + fg.rs)
+        fmt_value = input_taker(val_count)
+        
     return fmt_value
 
 def addition(val_1, val_2):
@@ -30,5 +38,14 @@ def operation_selector():
         '/' : division
     }
 
-    return operation_map[operation]
+    selected_operation = ''
+
+    try :
+        selected_operation = operation_map[operation]
+    except : 
+        print(fg.red + 'So, you are one of those who likes to try different stuff, huh?' + fg.rs)
+        print(fg.red + 'I\'m not gonna get you away with this, enter the right operation'  + fg.rs)
+        selected_operation = operation_selector()
+
+    return selected_operation
 
